3cf2035547f6bab7c1bf69bcd44affd96f78479a
Adarsh Singh <adarshsince2004@gmail.com>
2025-09-18 19:20:50 +0530

    hello world

---
+++ 1 Basics/first.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+
+int main() {
+    std::cout << "Hello, World!"<<"\n"<<"my first prog"<<"\n";
+    std::cout << "This project uses the C++17 language standard version."<<std::endl;
+    std::cout<<"adarsh";
+    return 0;
+}
