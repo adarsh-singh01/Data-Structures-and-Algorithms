5f07ad7d13bdacf101e2b52c2769d840b8f83539
Adarsh Singh <adarshsince2004@gmail.com>
2025-09-26 02:00:11 +0530

    more on vector,list and deque

--- 1 Learn the Basics/3 STL/vectors.cpp
+++ 1 Learn the Basics/3 STL/vectors.cpp
@@ -1,68 +1,139 @@
 #include <iostream>
 #include <vector>
+#include <list>
+#include <deque>
+
 using namespace std;
-//#include <vector> //for vector
 
-void explainVector()//a vector is a dynamic array...it can grow and shrink in size
+void explainVector()//a vector is a dynamic array that can resize itself automatically when an element is added or removed.
 {
-    vector<int> v; //vector of zero length..its a empty container
-    v.push_back(1); //{1}
-    v.emplace_back(2); //{1,2}
+    vector<int> v; // empty vector
+    v.push_back(1); // {1}
+    v.emplace_back(2); // {1, 2}
 
-    cout << "Vector at 1: " << v[1] << endl; //2
-    cout << "Vector at 0: " << v.at(0) << endl; //1
+    vector<pair<int, int>> vec; // vector of pairs
+    vec.push_back({1, 2});
+    vec.emplace_back(3, 4);
 
-    cout << "Front: " << v.front() << endl; //1
-    cout << "Back: " << v.back() << endl; //2
+    vector<int> v1(5, 100); // {100, 100, 100, 100, 100}
+    vector<int> v2(v1);     // copy of v1
+    vector<int> v3(5);      // {0, 0, 0, 0, 0}
 
-    cout << "Size: " << v.size() << endl; //2
+    cout << "Vector at 1: " << v[1] << endl;       // 2
+    cout << "Vector at 0: " << v.at(0) << endl;    // 1
+    cout << "Front: " << v.front() << endl;        // 1
+    cout << "Back: " << v.back() << endl;          // 2
+    cout << "Size: " << v.size() << endl;          // 2
 
-    v.pop_back(); //removes last element {1}
-    cout << "Size after pop: " << v.size() << endl; //1
+    v.pop_back(); // removes 2
+    cout << "Size after pop: " << v.size() << endl; // 1
 
-    //Iterating over the vector
-    cout << "Vector elements: ";
-    for (int i = 0; i < v.size(); i++)
+    // -----------------------------
+    // ITERATORS :iterators are used to point to the memory addresses of STL containers.
+    // -----------------------------
+
+    vector<int> w = {10, 20, 30, 40, 50};
+
+    // 'it' is a normal iterator to the first element
+    auto it = w.begin(); // same as: vector<int>::iterator it = w.begin();
+    cout << "First element: " << *it << endl; // 10
+    it++; // moves to second element
+
+    // 'it2' points to one-past-the-last element (should not be dereferenced)
+    auto it2 = w.end(); // same as: vector<int>::iterator
+
+    // 'it3' is a reverse iterator pointing BEFORE the first element
+    auto it3 = w.rend(); // same as: vector<int>::reverse_iterator
+
+    // 'it4' is a reverse iterator pointing to the LAST element
+    auto it4 = w.rbegin(); // same as: vector<int>::reverse_iterator
+
+    cout << "Last element using reverse iterator: " << *it4 << endl; // 50
+
+
+    cout<<w.back()<<endl; // 10
+    cout<<w.front()<<endl; // 50
+
+    for (vector<int>::iterator it = w.begin(); it != w.end(); it++)
     {
-        cout << v[i] << " ";
+        cout << *it << " "; // prints all elements
     }
-    cout << endl;
 
-    //Using iterators
-    cout << "Using iterators: ";
-    for (auto it = v.begin(); it != v.end(); it++)
+    for (auto it = w.begin(); it != w.end(); it++)//auto is used to automatically deduce the type of the iterator
     {
-        cout << *it << " ";
+        cout << *it << " "; // prints all elements
     }
-    cout << endl;
 
-    //Using for each loop
-    cout << "Using for each loop: ";
-    for (int value : v)
+    for (auto it : w) // range-based for loop
     {
-        cout << value << " ";
+        cout << it << " "; // prints all elements
     }
-    cout << endl;
 
-    //2D Vector
-    vector<vector<int>> vec2d;
-    vec2d.push_back({1, 2});
-    vec2d.emplace_back(vector<int>{3, 4});
+    //ERASE
+    w.erase(w.begin() + 1); // removes second element (20)
 
-    cout << "2D Vector elements: ";
-    for (const auto &row : vec2d)
-    {
-        for (int elem : row)
-        {
-            cout << elem << " ";
-        }
-        cout << "| ";
-    }
-    cout << endl;
+    w.erase(w.begin() + 2, w.begin() + 4); // removes elements at index 2 and 3 (30, 40)
+
+    //INSERT
+    vector<int> x(2, 100); // {100, 100}
+    x.insert(x.begin(), 300); // {300, 100, 100}
+    x.insert(x.begin() + 1, 2, 10); // {300, 10, 10, 100, 100}
+    vector<int> copy(2, 50); // {50, 50}
+    x.insert(x.begin(), copy.begin(), copy.end()); // {50, 50, 300, 10, 10, 100, 100}
+
+    vector<int> z1 = {10,20};
+    cout <<z1.size(); // 2
+
+    //z1.pop_back(); // removes last element
+
+    //z1-->{10,20}
+    //z2-->{30,40}
+
+    /*
+    z1.swap(z2); // swaps contents of z1 and z2
+    z1.clear(); // clears all elements from z1
+
+    cout << z1.empty(); // checks if z1 is empty
+    */
+    
 }
+
+
+void explainList(){//a list is a doubly linked list that allows efficient insertion and deletion of elements from both ends and from the middle.
+ // LIST
+    list <int> l;
+    l.push_back(1); // {1}
+    l.push_front(2); // {2, 1}
+    l.emplace_back(3); // {2, 1, 3}
+    l.emplace_front(4); // {4, 2, 1, 3}
+    l.pop_back(); // {4, 2, 1}
+
+    //Rest of the operations are same as vector
+    //begin(), end(), rbegin(), rend(), insert(), erase(), size(), clear(), empty()
+}
+
+void explainDeque()
+    {
+        deque<int> d;
+        d.push_back(1); // {1}
+        d.push_front(2); // {2, 1}
+        d.emplace_back(3); // {2, 1, 3}
+        d.emplace_front(4); // {4, 2, 1, 3}
+        d.pop_back(); // {4, 2, 1}
+        d.pop_front(); // {2, 1}
+
+        cout << "Front: " << d.front() << endl; // 2
+        cout << "Back: " << d.back() << endl;   // 1
+        cout << "Size: " << d.size() << endl;   // 2
+
+       // Rest of the operations are same as vector
+       // begin(), end(), rbegin(), rend(), insert(), erase(), size(), clear(), empty()
+    }
 
 int main()
 {
     explainVector();
+    explainList();
+    explainDeque();
     return 0;
 }
