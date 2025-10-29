8220784e53069f494d4082c15411a1384f5bc780
Adarsh Singh <adarshsince2004@gmail.com>
2025-10-29 23:56:32 +0530

    oop 3

---
+++ Kunal Kushwaha/07-OOP/03.java
@@ -0,0 +1,7 @@
+/*
+there are 4 OOPs concepts:
+1. Abstraction: Hiding the implementation details and showing only the functionality.
+2. Encapsulation: Bundling the data and methods that operate on the data within one unit, e.g., a class.
+3. Inheritance: Mechanism where one class acquires the properties (methods and fields) of another class.
+4. Polymorphism: Ability of a variable, function or object to take on multiple forms.
+ */
